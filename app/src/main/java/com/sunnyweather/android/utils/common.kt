861241001d8a926d29e4d85f89fce52abc6040fb
super0.net <superseller@super0.net>
2020-06-16 16:45:32 +0800

    框架搭建

---
+++ app/src/main/java/com/sunnyweather/android/utils/common.kt
@@ -0,0 +1,13 @@
+package com.sunnyweather.android.utils
+
+/**
+ *    @projectName: SunnyWeather
+ *    @ClassName : common
+ *    author : kpa
+ *    e-mail : kpa@super0.net
+ *    @CreateDate   : 2020/6/1510:49 AM
+ *    @UpdateDate   : 2020/6/1510:49 AM
+ *    desc   :
+ *    @UpdateRemark : 更新说明
+ *    version: 1.0
+ */
--- app/src/main/java/com/sunnyweather/android/widget/NavLayout.kt
+++ app/src/main/java/com/sunnyweather/android/widget/NavLayout.kt
@@ -1,15 +1,18 @@
 package com.sunnyweather.android.widget
 
 import android.content.Context
-import android.graphics.drawable.Drawable
+import android.graphics.Color
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.sunnyweather.android.R
+import com.sunnyweather.android.utils.getScreenWidth
 
 /**
  *    @projectName: SunnyWeather
@@ -26,6 +29,9 @@ class NavLayout : ConstraintLayout {
     private var mList = arrayListOf<Nav>()
     private var adapter: NavLayoutAdapter = NavLayoutAdapter(mList)
     private var onNavSelectedListener: OnNavSelectedListener? = null
+    private var navBackground = Color.WHITE
+    private var normalColor = Color.BLACK
+    private var selectColor = Color.BLUE
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -33,7 +39,14 @@ class NavLayout : ConstraintLayout {
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        val ta = context?.obtainStyledAttributes(attrs, R.styleable.NavLayout)
+        navBackground =
+            ta?.getColor(R.styleable.NavLayout_navBackground, Color.WHITE) ?: Color.WHITE
+        normalColor = ta?.getColor(R.styleable.NavLayout_normalColor, Color.BLACK) ?: Color.BLACK
+        selectColor = ta?.getColor(R.styleable.NavLayout_selectColor, Color.BLUE) ?: Color.BLUE
+        ta?.recycle()
+    }
 
     init {
         /**
@@ -42,12 +55,13 @@ class NavLayout : ConstraintLayout {
         val view = LayoutInflater.from(context).inflate(R.layout.nav_layout, this)
         val navRV = view.findViewById<RecyclerView>(R.id.navRV)
         initRv(navRV)
-        bindEvent(navRV)
+        bindEvent()
     }
 
-    private fun bindEvent(navRV: RecyclerView?) {
-        checkNotNull(navRV) { "RecyclerView is null" }
+    private fun bindEvent() {
         adapter.setOnItemClickListener { adapter, view, position ->
+            Log.e("pos", "pos = $position")
+            Toast.makeText(context, "fsfdsfdsfsd", Toast.LENGTH_LONG).show()
             checkNotNull(onNavSelectedListener) { "NavLayout.OnNavSelectedListener is null" }
             onNavSelectedListener?.onNavSelected(position)
         }
@@ -63,6 +77,16 @@ class NavLayout : ConstraintLayout {
 
     fun bindToViewPager(position: Int) {
         // 切换壮体
+        this.mList.size
+        for (i in 0 until this.mList.size) {
+            this.mList[i].isSelect = position == i
+        }
+        adapter.notifyDataSetChanged()
+    }
+
+    fun addData(navList: MutableList<Nav>) {
+        this.mList.clear()
+        this.mList.addAll(navList)
         adapter.notifyDataSetChanged()
     }
 
@@ -76,17 +100,34 @@ class NavLayout : ConstraintLayout {
         fun onNavSelected(position: Int)
     }
 
-    class NavLayoutAdapter(data: MutableList<Nav>) :
-        BaseQuickAdapter<Nav, BaseViewHolder>(R.layout.nav_layout, data) {
+    inner class NavLayoutAdapter(data: MutableList<Nav>) :
+        BaseQuickAdapter<Nav, BaseViewHolder>(R.layout.nav_item, data) {
         override fun convert(holder: BaseViewHolder, item: Nav) {
-
+            val screenWidth = getScreenWidth()
+            val width = screenWidth / data.size
+            val navItem = holder.getView<LinearLayout>(R.id.navItem)
+            val navIv = holder.getView<ImageView>(R.id.navIv)
+            val navName = holder.getView<TextView>(R.id.navName)
+            val layoutParams = navItem.layoutParams
+            layoutParams.width = width
+            navItem.layoutParams = layoutParams
+            val drawable = context.getDrawable(R.drawable.selector_nav_picture)
+            navIv.setImageDrawable(drawable)
+            navName.text = item.text
+            navItem.setBackgroundColor(navBackground)
+            if (item.isSelect) {
+                navName.setTextColor(selectColor)
+            } else {
+                navName.setTextColor(normalColor)
+            }
         }
+
 
     }
 
     class Nav(
-        val icon: Drawable,
+        val icon: Int,
         val text: String,
-        val isSelect: Boolean
+        var isSelect: Boolean
     )
 }
